ef0cd6fac943ccd5ac0cdafc4cd0104cb9c3fcea
rexsteroxy <rexsteroxy22@gmail.com>
2019-12-14 14:10:54 +0100

    worked on progressive web

---
+++ app.js
@@ -0,0 +1,30 @@
+const input = document.getElementById('lbsInput');
+const output = document.getElementById('output');
+
+output.style.visibility = 'hidden';
+
+input.addEventListener('input', (e) => {
+  let lbs = e.target.value;
+
+  output.style.visibility = 'visible';
+
+  let grams = document.getElementById('gOutput');
+  grams.innerHTML = lbs/0.0022046;
+
+  let kiloGrams = document.getElementById('kgOutput');
+  kiloGrams.innerHTML = lbs/2.2046;
+
+  let ounces = document.getElementById('ozOutput');
+  ounces.innerHTML = lbs*16;
+
+  e.preventDefault();
+});
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js').then( () => {
+      console.log('Service Worker Registered')
+    })
+  })
+}
