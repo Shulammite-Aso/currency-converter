5198ed7aa21155693698e34c9222a4aa14a261f2
Shulammite-Aso <asoamarachi@gmail.com>
2019-12-24 21:13:02 +0100

    change app from weight converter to currency converter

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,18 @@
 const input = document.getElementById('lbsInput');
 const output = document.getElementById('output');
 
+//service worker registration
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(){
+    console.log('service worker registered');
+  }, function (err) {
+    console.log('sw failed to install', err)
+  })
+}
+
+
 output.style.visibility = 'hidden';
 input.addEventListener('input', (e) => {
   let lbs = e.target.value;
@@ -8,13 +20,13 @@ input.addEventListener('input', (e) => {
   output.style.visibility = 'visible';
 
   let grams = document.getElementById('gOutput');
-  grams.innerHTML = lbs/0.0022046;
+  grams.innerHTML = lbs/362.021;
 
   let kiloGrams = document.getElementById('kgOutput');
-  kiloGrams.innerHTML = lbs/2.2046;
+  kiloGrams.innerHTML = lbs/401.363;
 
   let ounces = document.getElementById('ozOutput');
-  ounces.innerHTML = lbs*16;
+  ounces.innerHTML = lbs/469.244;
 
   e.preventDefault();
 });
---
+++ public/sw.js
@@ -0,0 +1,99 @@
+const URLsToCache = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/css/app.css',
+    '/img/favicon.png',
+    '/img/icons/app-icon-144x144.png',
+    'https://fonts.googleapis.com/css?family=Roboto+Mono',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', function (event) {
+    console.log('sw installed');
+    event.waitUntil(
+
+        caches.open('static')
+            .then(function (cache) {
+                console.log('Opened cache');
+                cache.addAll(URLsToCache)
+            })
+    )
+
+
+});
+
+
+
+
+self.addEventListener('activate', function () {
+    console.log('sw activated');
+});
+
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
+
+/*self.addEventListener('install', function(event){
+    console.log('sw installed')
+    event.waitUntil(
+        caches.open('static')
+        .then(function (cache){
+            cache.addAll([
+                '/',
+                'index.html',
+                '/js/app.js',
+                '/css/app.css',
+                'img/favicon.png',
+                '/img/icons/app-icon-144X144.png',
+                'https://fonts.googleapis.com/css?family=Roboto+Mono',
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+            ])
+        })
+    )
+    
+});
+
+
+self.addEventListener('activate', function() {
+    console.log('sw activated');
+});
+
+self.addEventListener('fetch', function(event){
+    console.log('fetching sw...');
+    event.respondwith(
+        caches.match()
+    )
+})*/
